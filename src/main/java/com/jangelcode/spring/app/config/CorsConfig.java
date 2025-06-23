2a504a8143fb60949dfdfd4caa1c714a0ebeca63
Jose <jangelcm4@gmail.com>
2025-06-23 04:44:13 -0500

    agrega la url de producción de mi front para las cors

--- src/main/java/com/jangelcode/spring/app/config/CorsConfig.java
+++ src/main/java/com/jangelcode/spring/app/config/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") // Agrega aquí la URL de producción también
+                        .allowedOrigins("http://localhost:4200", "https://podology-diale.netlify.app") // Agrega aquí la
+                                                                                                       // URL de
+                                                                                                       // producción
+                                                                                                       // también
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
